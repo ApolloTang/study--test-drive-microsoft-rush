0b8fb48cfec65421bd4a33216bf06390011fd948
Apollo Tang <tang.apollo@gmail.com>
2021-05-04 21:43:16 -0400

    fix typescript import image

--- apps/web3/src/app/index.tsx
+++ apps/web3/src/app/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import img from './react.png';
+import s from './style.module.less';
 import MyButton from '@mylibs/ui--button2';
 
 console.log('fdsaf')
 const App = () => (
-  <div>
+  <div className={`${s.app}`}>
     <h1>Hello React</h1>
     <div><MyButton>mybutton</MyButton> </div>
+    <div className={`${s.imageContainer}`}><img src={img} /></div>
   </div>
 );
 
